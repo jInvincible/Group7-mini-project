d8c2b682fa3013a08bba903de82cb98227a02e2a
jInvincible <nguyensyhainam1986@gmail.com>
2022-09-13 21:23:35 +0700

    allocate raw file sources

--- data_understanding.py
+++ data_understanding.py
@@ -1,10 +1,12 @@
 import pandas as pd
+from glob import glob
 source_path =  'raw data from kaggle'
 filename = 'social media influencers - instagram.csv'
 file_path = source_path + '/' + filename
 
-df = pd.read_csv(file_path, engine='python')
-df.info()
+filename_list = glob(source_path +'/*.csv')
 
-df.head()
-df.tail()
+df = pd.read_csv(file_path, engine='python')
+df_columns = df.columns.split()
+df.columns.replace(to_replace=r'\s+$', value=r'', regex=True)
+
