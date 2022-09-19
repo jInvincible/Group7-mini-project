36b72a4d8049818ddd5682113b05e1a2bf1b16b8
APTECH <nguyensyhainam1986@gmail.com>
2022-09-19 19:52:28 +0700

    .

--- raw data/collect data from wikipedia.py
+++ raw data/collect data from wikipedia.py
@@ -5,18 +5,21 @@ import pandas as pd
 import numpy as np
 
 # variables
-default_round_list = ['Group 1', 'Group 2', 'Group 3', 'Group 4', 'Group 5',
+# group_list sample
+default_group_list = ['Group 1', 'Group 2', 'Group 3', 'Group 4', 'Group 5',
                       'Group 6', 'Group 7', 'Group 8', 'Group A', 'Group B',
                       'Group C', 'Group D', 'Group E', 'Group F', 'Group G',
-                      'Group H', 'Semi-finals', 'Final', 'Preliminary round',
-                      'Quarter-finals', 'Semi-finals', 'Match for third place',
-                      'First round','Round of 16', 'Third place',
-                      'Play-off for third place'
+                      'Group H'
                       ]
 
-# url
+# knockout_list sample
+default_knockout_list = ['Semi-finals', 'Final', 'Preliminary round',
+                         'Quarter-finals', 'Semi-finals', 'Match for third place',
+                         'First round', 'Round of 16', 'Third place',
+                         'Play-off for third place']
+
+# url summary page
 url = 'https://en.wikipedia.org/wiki/2018_FIFA_World_Cup'
-#https://en.wikipedia.org/wiki/2018_FIFA_World_Cup_Group_A
 
 # request content
 r = requests.get(url)
@@ -24,9 +27,26 @@ r = requests.get(url)
 # parse HTML code
 soup = bs4.BeautifulSoup(r.text, 'html.parser')
 
-# create round_list
-headline = soup.find_all(attrs='mw-headline', text=default_round_list)
-round_list = [round.text for round in headline]
+# create group_list
+headline_group = soup.find_all(attrs='mw-headline', text=default_group_list)
+group_list = [group.text for group in headline_group]
+
+headline_knockout = soup.find_all(attrs='mw-headline', text=default_knockout_list)
+knockout_list = [rounds.text for rounds in headline_knockout]
+
+# check number of matches each group
+url_group = [f'{url}/{group}'.replace(' ', '_') for group in group_list]
+dic = dict.fromkeys(group_list, [])
+
+url_groupA ='https://en.wikipedia.org/wiki/2018_FIFA_World_Cup/Group_A'
+r_A = requests.get(url_groupA)
+soup_A = bs4.BeautifulSoup(r_A.text, 'html.parser')
+match_count_A = soup_A.find_all(name='table', attrs='wikitable')
+
+
+
+# create dictionary with key = group and value = list of matches
+
 
 # date and time
 date_f = soup.find_all(attrs='bday dtstart published updated')
