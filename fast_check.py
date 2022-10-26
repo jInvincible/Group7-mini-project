4289e6dfc2a9305c2ca4498b965e17b6dd9288fb
jInvincible <nguyensyhainam1986@gmail.com>
2022-10-26 16:19:17 +0700

    new watson_healthcare_attrition.pt version

--- fast_check.py
+++ fast_check.py
@@ -1,27 +1,12 @@
-class Solution(object):
-    def romanToInt(self, s):
-        symbol_dic = {'I': 1, 'V': 5,
-                      'X': 10, 'L': 50,
-                      'C': 100, 'D': 500,
-                      'M': 1000                     
-                     }
-        result = 0
-        i = 0
-        while i <= len(s):
-            try:
-                letter = s[i]
-                right_letter = s[i+1]
-                if symbol_dic[letter] >= symbol_dic[right_letter]:
-                    result += symbol_dic[letter]
-                    i+=1
-                else:
-                    result += symbol_dic[right_letter] - symbol_dic[letter]
-                    i+=2
-            except:
-                if s[-1] == letter:
-                        result += symbol_dic[s[-1]]
-                return result
-                    
-roman_letter = Solution()
-result = roman_letter.romanToInt('IV')
-print(result)
+main = 'BusinessTravel'
+title = ''
+for i in range(len(main)):
+    if i != 0:
+        if main[i].isupper():
+            title += f' {main[i]}'
+        else:
+            title += main[i]
+    else:
+        title += main[i]
+
+print(dir(list))
